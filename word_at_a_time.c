bf3b4b9cd496ae7e61f062263f97a61694c6ffe2
Emrah Akgül <45803976+akgulemrah@users.noreply.github.com>
2024-04-02 01:58:14 +0300

    Update word_at_a_time.c

--- word_at_a_time.c
+++ word_at_a_time.c
@@ -1,1 +1,30 @@
+#include "word_at_a_time.h"
+#include <stddef.h>
 
+int word_at_a_time(char *src, char *buf)
+{
+	static char *src_ptr = NULL;
+	if (src != NULL) {
+		src_ptr = src;
+	}
+
+	if (src_ptr == NULL || *src_ptr == '\0') {
+		return -1; 
+	}
+
+	while (*src_ptr == ' ' && *src_ptr != '\0') {
+		src_ptr++;
+	}
+
+	if (*src_ptr == '\0') {
+		return -1; 
+	}
+
+	while (*src_ptr != ' ' && *src_ptr != '\0') {
+		*buf++ = *src_ptr++;
+	}
+
+	*buf = '\0';
+
+	return 0;
+}
