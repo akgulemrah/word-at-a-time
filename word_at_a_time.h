3c8338399706c7226c55b00ebb6ae5195a414fd8
Emrah Akgül <45803976+akgulemrah@users.noreply.github.com>
2024-04-02 09:30:48 +0300

    Update word_at_a_time.h

--- word_at_a_time.h
+++ word_at_a_time.h
@@ -1,6 +1,32 @@
 #if !defined(WORD_AT_A_TIME_H)
 #define WORD_AT_A_TIME_H 1
 
-int word_at_a_time(char *src, char *buf);
+int word_at_a_time(char *src, char *buf)
+{
+        static char *src_ptr = NULL;
+        if (src != NULL) {
+                src_ptr = src;
+        }
+
+        if (src_ptr == NULL || *src_ptr == '\0') {
+                return -1; 
+        }
+
+        while (*src_ptr == ' ' && *src_ptr != '\0') {
+                src_ptr++;
+        }
+
+        if (*src_ptr == '\0') {
+                return -1; 
+        }
+
+        while (*src_ptr != ' ' && *src_ptr != '\0') {
+                *buf++ = *src_ptr++;
+        }
+
+        *buf = '\0';
+
+        return 0;
+}
 
 #endif /* WORD_AT_A_TIME */
